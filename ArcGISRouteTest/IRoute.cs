b1462a6a9ace778a4d55654ebf3f1d8c7ae577fd
ATangeman <atangeman@sandiego.gov>
2019-03-05 10:10:49 -0800

    Refactored into class wrapper object

---
+++ ArcGISRouteTest/IRoute.cs
@@ -0,0 +1,63 @@
+﻿
+namespace ArcGISRouteTest
+{
+    using System;
+
+    public interface RouteResult
+    {
+
+        TimeSpan EndTimeShift { get; }
+
+        //
+        //     Gets route's name.
+        string RouteName { get; }
+        //
+        // Summary:
+        //     Gets start time in UTC.
+        //
+        // Remarks:
+        //     To calculate the departure time for the timezone of the start location, add the
+        //     Esri.ArcGISRuntime.Tasks.NetworkAnalysis.Route.StartTimeShift. Note that this
+        //     might be a different timezone than System.DateTimeKind.Local if the start location
+        //     is in a different timezone than the device. You can also use System.DateTime.ToLocalTime
+        //     to get the arrival time in the time zone of the device.
+        DateTimeOffset? StartTime { get; }
+        //
+        // Summary:
+        //     Gets the time-zone shift for the start time.
+        //
+        // Remarks:
+        //     The shift is the amount to apply to the Esri.ArcGISRuntime.Tasks.NetworkAnalysis.Route.StartTime
+        //  // Summary:
+           to get the local time of the start location.
+        TimeSpan StartTimeShift { get; }
+
+        //
+        // Summary:
+        //     Gets total route length in meters.
+        double TotalLength { get; }
+        //
+        // Summary:
+        //     Gets route total time.
+        //
+        // Remarks:
+        //     This includes any travel time, time spent waiting at stops (arriving before the
+        //     start of a time window), and service time at stops.
+        TimeSpan TotalTime { get; }
+        //
+        // Summary:
+        //     Gets total travel time.
+        //
+        // Remarks:
+        //     This includes only time of the travel.
+        TimeSpan TravelTime { get; }
+        //
+        // Summary:
+        //     Gets the total amount of additional time incurred due to time window violations.
+       TimeSpan ViolationTime { get; }
+        //
+        // Summary:
+        //     Gets the total amount of additional time incurred due to waiting at time windows.
+        TimeSpan WaitTime { get; }
+    }
+}
--- ArcGISRouteTest/Program.cs
+++ ArcGISRouteTest/Program.cs
@@ -21,43 +21,10 @@ namespace ArcGISRouteTest
 
         private static async void RunRouteTask()
         {
-            var routeSourceUri = new Uri(RouteService);
-            var routeTask = await RouteTask.CreateAsync(routeSourceUri);
-
-            // get the default route parameters
-            var routeParams = await routeTask.CreateDefaultParametersAsync();
-            // explicitly set values for some params
-            routeParams.ReturnDirections = true;
-            routeParams.ReturnRoutes = true;
-            routeParams.OutputSpatialReference = new Esri.ArcGISRuntime.Geometry.SpatialReference(102100);
-
-            // create a Stop for my location
-            var myLocation = new MapPoint(-13034379.7137452, 3858390.14190331, SpatialReferences.WebMercator);
-            var stop1 = new Stop(myLocation);
-
-            // create a Stop for your location
-            var yourLocation = new MapPoint(-13032763.2362459, 3860880.60006008, SpatialReferences.WebMercator);
-            var stop2 = new Stop(yourLocation);
-
-            // assign the stops to the route parameters
-            var stopPoints = new List<Stop> { stop1, stop2 };
-            routeParams.SetStops(stopPoints);
-
-            // create a PointBarrier to avoid
-            var accidentLocation = new MapPoint(-13033331.7103904, 3859368.53617918, SpatialReferences.WebMercator);
-            var barrier = new PointBarrier(accidentLocation);
-
-            // add the point barriers to the route parameters
-            List<PointBarrier> pointBarriers = new List<PointBarrier> { barrier };
-            routeParams.SetPointBarriers(pointBarriers);
-
-            var routeResult = await routeTask.SolveRouteAsync(routeParams);
-
-            // get the route from the results
-            var route = routeResult.Routes[0];
-
+            ArcGISRouteTool route = new ArcGISRouteTool(RouteService);
+            route.AddStop(-13034379.7137452, 3858390.14190331, 102100);
+            route.AddStop(-13032763.2362459, 3860880.60006008, 102100);
+            var result = await route.Solve();
         }
-
-
     }
 }
